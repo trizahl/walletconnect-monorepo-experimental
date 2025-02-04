459c41e90cdb05a7cb8b122d25e135df42d0cc62
trizahl <121090956+trizahl@users.noreply.github.com>
2025-02-04 05:37:52 +0100

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,7 +18,35 @@ export default defineConfig({
   },  define: {
     "process.env.IS_VITEST": true,
   },
+  test: {: {
+    "process.env.IS_VITEST": true,
+  },
   test: {
+    testTimeout: 800_000,
+    hookTimeout: 800_000,
+  },  define: {
+    "process.env.IS_VITEST": true,
+  },
+  test: {
+    testTimeout: 800_000,
+    hookTimeout: 800_000,
+  },  define: {
+    "process.env.IS_VITEST": true,
+  },: {
+    "process.env.IS_VITEST": true,
+  },
+  test: {
+    testTimeout: 800_000,
+    hookTimeout: 800_000,
+  },  define: {
+    "process.env.IS_VITEST": true,
+  },
+  test: {
+    testTimeout: 800_000,
+    hookTimeout: 800_000,
+  },  define: {
+    "process.env.IS_VITEST": true,
+  },
     testTimeout: 800_000,
     hookTimeout: 800_000,
   },  define: {
